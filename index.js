cf6650bf78bd53de49fe4fca089069005c737ca8
vijayakumar58 <vijayalingam58@gmail.com>
2023-11-18 15:28:25 +0530

    add

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+import { userRouter } from './routes/user.js';
+import { recipesRouter } from './routes/recipes.js'
+const URL = process.env.DB
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:3001"
+}));
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
